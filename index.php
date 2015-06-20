72ee3535959cd59c737e099f63fd4c145ca0cf71
goliath309 <roll.93@hotmail.com>
2015-06-20 02:26:32 -0500

    avance pagina ruben :family:

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE HTML>
+
+<html>
+	<header>
+		<link rel="icon" type="image/png" href="images/ico.png" />
+		<title>LERU</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0;" />  
+		<link href="estilo.css" rel="stylesheet" type="text/css" />
+	</header>
+	<body>
+		<script src="JS/apareceMensajes.js"></script>
+		<div id="head">
+			<div id="contenidoHead">
+				<div id="areaLogoHead" onmouseover="mensajeIcono1()" onmouseout="noMensajeIcono1()">
+					<a href="index.php">
+						<img id="logoHead" src="images/logoHeader.png">		
+						<h4>LERU-TECH</h4>						
+					</a>
+				</div>	
+				<div id="mensajeIcono1" class="mensajeIcono centrar">Página principal</div>				
+			</div>
+		</div>
+		
+		<div id="contenido">
+		</div>
+		
+		<div id="footer">
+			<div id="contenidoFooter">
+				<div id="redesSociales">
+					<img class="iconoRedSocial" src="images/google+.png">
+					<img class="iconoRedSocial" src="images/youtube.png">
+					<img class="iconoRedSocial" src="images/twitter.png">
+					<a class="cirucloBlanco" href="https://www.facebook.com"><img class="iconoRedSocial"src="images/facebook.png"></a>				
+				</div>
+				<a href="https://www.google.com.mx">
+					<h4>Contactanos</h4>
+				</a>
+			</div>
+		</div>
+	</body>
+</html>
